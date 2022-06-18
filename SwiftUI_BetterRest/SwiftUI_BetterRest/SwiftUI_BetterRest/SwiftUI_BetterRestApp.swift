544e8edb319349cc293781a21f64d783b957ed11
atokarxd <93882197+atokarxd@users.noreply.github.com>
2022-06-18 19:00:15 +0200

    Add files via upload

---
+++ SwiftUI_BetterRest/SwiftUI_BetterRest/SwiftUI_BetterRest/SwiftUI_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BetterRestApp.swift
+//  SwiftUI_BetterRest
+//
+//  Created by Atokarxd on 2022. 06. 18..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
