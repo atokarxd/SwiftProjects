fd709e4eccc34be20eab7f31c8e284985384c2bc
atokarxd <93882197+atokarxd@users.noreply.github.com>
2022-06-20 17:41:59 +0200

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Atokarxd on 2022. 06. 20..
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
