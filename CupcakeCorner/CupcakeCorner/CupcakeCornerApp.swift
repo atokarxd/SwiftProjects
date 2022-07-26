8575ea66137475a93111743745b3532686eb76e9
atokarxd <93882197+atokarxd@users.noreply.github.com>
2022-07-26 11:34:20 +0200

    Add files via upload

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Atokarxd on 2022. 07. 25..
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
