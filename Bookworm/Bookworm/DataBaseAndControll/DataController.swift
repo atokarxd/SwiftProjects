f0f0d6aac8d0f92cf34627c35601a1aacd5deb51
atokarxd <93882197+atokarxd@users.noreply.github.com>
2022-07-27 10:06:26 +0200

    Add files via upload

---
+++ Bookworm/Bookworm/DataBaseAndControll/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Atokarxd on 2022. 07. 26..
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
